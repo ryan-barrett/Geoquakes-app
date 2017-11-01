d96235179fe7dab0534dd31a27685a24cae5a26f
ryan-barrett <ryan.barrett@spookyhouse.org>
2017-10-31 21:44:45 -0700

    added api call

--- scripts/app.js
+++ scripts/app.js
@@ -1,45 +1,69 @@
 // define globals
-var weekly_quakes_endpoint =
-  "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
-var mapCords = [];
 
+//define endpoint
+var dailyQuakes =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
+
+//define map
+var map;
+
+//when info is gathered
 $(document).ready(function() {
-  console.log("Let's get coding!");
+  //callback for listing quake data/markers
+  listQuakeData();
+});
+
+//function for gathering quake locations and marker settings
+function listQuakeData(dataResponse) {
   $.ajax({
     method: "GET",
-    url: weekly_quakes_endpoint,
+    url: dailyQuakes,
     dataType: "json",
-    //data:responseData,
-    success: onSuccess
-  }); //closes ajax
+    success: function(dataResponse) {
+      // console.log(dataResponse);
 
-  function onSuccess(responseData) {
-    var x = responseData;
-    quakeArray = x.features;
-    Array.from(x).forEach(function(quake) {
-      console.log("test");
+      //defining unnamed Object as variable
+      var earthquakes = dataResponse;
 
-      var title = quake.properties.title;
-      var hours_ago = Math.round(
-        (Date.now() - quake.properties.time) / (1000 * 60 * 60)
-      );
-      $info_row_target.append(
-        "<p>" + title + " / " + hours_ago + " hours ago</p>"
-      );
-      var lat = quake.geometry.coordinates[1];
-      var lng = quake.geometry.coordinates[0];
-      new google.maps.Marker({
-        position: new google.maps.LatLng(lat, lng),
-        map: map,
-        title: title
+      //for each location, create a list item and marker
+      earthquakes.features.forEach(function listAndMarker(quake) {
+        //List item of earthquake info
+        var title = quake.properties.title;
+        var hoursAgo = Math.round(
+          (Date.now() - quake.properties.time) / 3600000
+        );
+        $(".earthquakeData").append(
+          `<li>${hoursAgo} hours ago | ${title}</li>`
+        );
+
+        //Create Marker
+        var lat = quake.geometry.coordinates[1];
+        var lng = quake.geometry.coordinates[0];
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(lat, lng),
+          map: map,
+          title: title
+        });
       });
-    });
-
-    for (i = 0; i < responseData.features.length; i++) {
-      $("h2").append(`${responseData.features[i].properties.title}`);
     }
-  }
-}); //closes documentReady
+  });
+}
 
-// function updateGlobal(cords){
-// mapCords = cords;
+//function for creating map
+function createMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 37.78,
+      lng: -122.44
+    },
+    zoom: 3
+  });
+  // var homeMarker = new google.maps.Marker({
+  //   position: {
+  //     lat: 37.78,
+  //     lng: -122.44
+  //   },
+  //   map: map,
+  //   title: "San Francisco (no earthquake)"
+  // });
+}
